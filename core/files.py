c1a6d86cb6d5708ec77f708671425db5809b6625
vkotronis <biece89@gmail.com>
2017-11-17 11:40:36 +0200

    creating log dir automatically for files.py

--- core/files.py
+++ core/files.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import time
 import json
+import os
 
 class WriteLogs():
 
@@ -12,12 +13,15 @@ class WriteLogs():
 		self.date = time.strftime("%d-%m-%Y")
 		self.service = service
 		self.monitor = monitor
-		self.prefix =prefix
+		self.prefix = prefix
 		self.filename = str(monitor) + "_" + str(prefix) + "_" + str(self.date)
+		if not os.path.isdir(self.path):
+			os.mkdir(self.path)
 
 
 	def set_filename(self, prefix):		
 		self.filename = str(self.monitor) + "_" + str(prefix) + "_" + str(self.date)
+
 
 	def set_date(self):
 		self.date = time.strftime("%d-%m-%Y")

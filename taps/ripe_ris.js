36fe94a486a9fb21e6aab75bd4844f70b1110171
slowr <dimitris@opennetworking.org>
2018-03-21 17:40:25 +0200

    fixed RIPE RIS monitor to send only wanted prefixes

--- taps/ripe_ris.js
+++ taps/ripe_ris.js
@@ -31,19 +31,50 @@ parser.addArgument(
 
 var args = parser.parseArgs();
 
-socket.on('ris_message', function(msg) {
-    json_obj = {
-        'timestamp': msg['timestamp'],
-        'prefix': msg['prefix'],
-        'service': 'RIPEris '.concat(msg['host']),
-        'as_path': msg['path'],
-        'type': msg['type'],
-    };
+var ip2long = function(ip){
+    var components;
 
-    client.queryMformat(json_obj, function(err, response) {
-        if(err)
-	       console.log(err);
-    });
+    if(components = ip.match(/^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$/))
+    {
+        var iplong = 0;
+        var power  = 1;
+        for(var i=4; i>=1; i-=1)
+        {
+            iplong += power * parseInt(components[i]);
+            power  *= 256;
+        }
+        return iplong;
+    }
+    else return -1;
+};
+
+var inSubNet = function(ip, subnet)
+{   
+    var mask, base_ip, long_ip = ip2long(ip);
+    if( (mask = subnet.match(/^(.*?)\/(\d{1,2})$/)) && ((base_ip=ip2long(mask[1])) >= 0) )
+    {
+        var freedom = Math.pow(2, 32 - parseInt(mask[2]));
+        return (long_ip > base_ip || long_ip === base_ip) && 
+            ((long_ip < base_ip + freedom - 1) || (long_ip === base_ip + freedom - 1));
+    }
+    else return false;
+};
+
+socket.on('ris_message', function(msg) {
+    if(inSubNet(msg['prefix'].split('/')[0], args.prefix)){
+        json_obj = {
+            'timestamp': msg['timestamp'],
+            'prefix': msg['prefix'],
+            'service': 'RIPEris '.concat(msg['host']),
+            'as_path': msg['path'],
+            'type': msg['type'],
+        };
+
+        client.queryMformat(json_obj, function(err, response) {
+            if(err)
+    	       console.log(err);
+        });
+    }
 });
 
 socket.emit('ris_subscribe', 

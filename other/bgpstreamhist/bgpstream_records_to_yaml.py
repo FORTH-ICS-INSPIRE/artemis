c67d3235c8fdead0591f1707e280514c73d3731e
vkotronis <biece89@gmail.com>
2018-08-03 18:53:01 +0300

    cleaning up the code of record to yaml conf transformation

--- other/bgpstreamhist/bgpstream_records_to_yaml.py
+++ other/bgpstreamhist/bgpstream_records_to_yaml.py
@@ -5,6 +5,7 @@ import csv
 import argparse
 import ruamel.yaml
 
+
 def __remove_prepending(seq):
     last_add = None
     new_seq = []
@@ -18,6 +19,7 @@ def __remove_prepending(seq):
         is_loopy = True
     return (new_seq, is_loopy)
 
+
 def __clean_loops(seq):
     # use inverse direction to clean loops in the path of the traffic
     seq_inv = seq[::-1]
@@ -30,19 +32,22 @@ def __clean_loops(seq):
             new_seq_inv = new_seq_inv[:x_index+1]
     return new_seq_inv[::-1]
 
+
 def __clean_as_path(as_path):
     (clean_as_path, is_loopy) = __remove_prepending(as_path)
     if is_loopy:
         clean_as_path = __clean_loops(clean_as_path)
     return clean_as_path
 
-def as_mapper(asn_str):
+
+def __as_mapper(asn_str):
     if asn_str != '':
         try:
             return int(asn_str)
         except:
             return 0
     return 0
+
 
 def parse_bgpstreamhist_csvs(input_dir=None):
 
@@ -69,7 +74,7 @@ def parse_bgpstreamhist_csvs(input_dir=None):
                     continue
                 typ = row[6]
                 if typ == 'A':
-                    as_path = list(map(as_mapper, row[3].split(',')))
+                    as_path = list(map(__as_mapper, row[3].split(',')))
                     as_path = __clean_as_path(as_path)
                     if len(as_path) > 0:
                         all_asns.add(as_path[-1])
@@ -82,15 +87,15 @@ def parse_bgpstreamhist_csvs(input_dir=None):
 
     return (prefixes, all_asns, prefix_pols)
 
+
 def create_prefix_defs(yaml_conf, prefixes):
     yaml_conf['prefixes'] = ruamel.yaml.comments.CommentedMap()
-    #print(dir(yaml_conf['prefixes']))
     for prefix in prefixes:
         prefix_str = 'prefix_{}'.format(prefix.replace('.', '_').replace('/', '_'))
         yaml_conf['prefixes'][prefix_str] = ruamel.yaml.comments.CommentedSeq()
         yaml_conf['prefixes'][prefix_str].append(prefix)
-        #print(dir(yaml_conf['prefixes'][prefix_str]))
         yaml_conf['prefixes'][prefix_str].yaml_set_anchor(prefix_str)
+
 
 def create_monitor_defs(yaml_conf):
     yaml_conf['monitors'] = ruamel.yaml.comments.CommentedMap()
@@ -105,6 +110,7 @@ def create_monitor_defs(yaml_conf):
     # 'exabgp': ['ip': '192.168.1.1', 'port': 5000, ...],
     # 'bgpstreamhist': '/home/vkotronis/Desktop/git_projects/artemis-tool/other/bgpstreamhist/test_dir2'
 
+
 def create_asn_defs(yaml_conf, asns):
     yaml_conf['asns'] = ruamel.yaml.comments.CommentedMap()
     for asn in asns:
@@ -112,9 +118,10 @@ def create_asn_defs(yaml_conf, asns):
         yaml_conf['asns'][asn_str] = ruamel.yaml.comments.CommentedSeq()
         yaml_conf['asns'][asn_str].append(asn)
         yaml_conf['asns'][asn_str].yaml_set_anchor(asn_str)
-#
+
+
 def create_rule_defs(yaml_conf, prefix_pols):
-    yaml_conf['rules'] = []
+    yaml_conf['rules'] = ruamel.yaml.comments.CommentedSeq()
     for prefix in prefix_pols:
         pol_dict = ruamel.yaml.comments.CommentedMap()
         prefix_str = 'prefix_{}'.format(prefix.replace('.', '_').replace('/', '_'))
@@ -123,6 +130,7 @@ def create_rule_defs(yaml_conf, prefix_pols):
         pol_dict['neighbors'] = [yaml_conf['asns']['AS{}'.format(asn)] for asn in prefix_pols[prefix]['neighbors']],
         pol_dict['mitigation'] = 'manual'
         yaml_conf['rules'].append(pol_dict)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='BGPStream Historical Monitor')
@@ -136,19 +144,23 @@ if __name__ == '__main__':
     (prefixes, asns, prefix_pols) = parse_bgpstreamhist_csvs(csv_dir)
 
     with open(args.yaml_file, 'w') as f:
+        # initial comments
         f.write('#\n')
         f.write('# ARTEMIS Configuration File\n')
         f.write('#\n')
         f.write('\n')
 
+        # initialize conf
         yaml = ruamel.yaml.YAML()
         yaml_conf = ruamel.yaml.comments.CommentedMap()
 
+        # populate conf
         create_prefix_defs(yaml_conf, prefixes)
         create_monitor_defs(yaml_conf)
         create_asn_defs(yaml_conf, asns)
         create_rule_defs(yaml_conf, prefix_pols)
 
+        # in-file comments
         yaml_conf.yaml_set_comment_before_after_key('prefixes',
                                                     before='Start of Prefix Definitions')
         yaml_conf.yaml_set_comment_before_after_key('monitors',
@@ -169,5 +181,8 @@ if __name__ == '__main__':
                                                     before='\n')
         yaml_conf.yaml_set_comment_before_after_key('rules',
                                                     before='Start of Rule Definitions')
+        # dump conf
         yaml.dump(yaml_conf, f)
+
+        # end comments
         f.write('# End of Rule Definitions\n')

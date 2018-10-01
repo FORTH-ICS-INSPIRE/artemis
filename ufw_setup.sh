059bcf3d8d72c410abc2e293e2a9d7ca88a693c5
vkotronis <biece89@gmail.com>
2018-10-01 10:27:43 +0300

    ufw_setup update

--- ufw_setup.sh
+++ ufw_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Install ufw
-apt-get -y install ufw
+# Install ufw (please do this off-script)
+# apt-get -y install ufw
 
 # Set default incoming policy
 ufw default deny incoming
@@ -13,29 +13,31 @@ ufw default allow outgoing
 # All related and established connections pass through
 # (by default this is enabled on ufw)
 
-# We can ssh to ARTEMIS only from the gateway VM
-ufw allow from <GW_VM_IP> to any port 22 proto tcp
+# We can ssh to ARTEMIS from certain access prefixes
+ufw allow from <SSH_ACCESS_PREFIX_1> to any port 22 proto tcp
+# ...
+ufw allow from <SSH_ACCESS_PREFIX_N> to any port 22 proto tcp
 
 # We can ping the machine from anywhere
 # (by default ufw enables ping requests)
 
 # We can use https from certain access prefixes
 # (incl. /32):
-ufw allow from <ACCESS_PREFIX_1> to any port 443 proto tcp
+ufw allow from <HTTPS_ACCESS_PREFIX_1> to any port 443 proto tcp
 # ...
-ufw allow from <ACCESS_PREFIX_N> to any port 443 proto tcp
+ufw allow from <HTTPS_ACCESS_PREFIX_N> to any port 443 proto tcp
 
 # [OPTIONAL] We can form i/eBGP sessions with one or more
 # local routers
-ufw allow from <ROUTER_1> to any port 179 proto tcp
+# ufw allow from <ROUTER_1> to any port 179 proto tcp
 # ...
-ufw allow from <ROUTER_N> to any port 179 proto tcp
+# ufw allow from <ROUTER_N> to any port 179 proto tcp
 
 # Anything is allowed on the local interface
 ufw allow in on lo to any
 
-# Enable ufw
-ufw enable
+# Enable ufw (please do this off-script)
+# ufw enable
 
 # [OPTIONAL] ufw disable, ufw status,
 # ufw status numbered, ufw help...

f42ff3f31cc47cb4fcfa7a0ccf72c8f0e4dde885
slowr <jim.mavrommatis@gmail.com>
2018-10-02 18:17:25 +0000

    added configuration for the logger

--- backend/utils/__init__.py
+++ backend/utils/__init__.py
@@ -1,24 +1,43 @@
-import json
 import os
 import time
 import logging
+import logging.handlers
+import logging.config
+import yaml
+
 
 if not os.path.exists('snapshots'):
     os.makedirs('snapshots')
+
+
+if not os.path.exists('logs'):
+    os.makedirs('logs')
+
 
 SYSLOG_HOST, SYSLOG_PORT = os.getenv('SYSLOG_HOST', 'localhost:514').split(':')
 RABBITMQ_HOST = os.getenv('RABBITMQ_HOST', 'localhost')
 MEMCACHED_HOST = os.getenv('MEMCACHED_HOST', 'localhost')
 
 
-def get_logger():
-    log = logging.getLogger('artemis_logger')
-    log.setLevel(logging.DEBUG)
-    handler = logging.handlers.SysLogHandler(address=(SYSLOG_HOST, int(SYSLOG_PORT)))
-    formatter = logging.Formatter('%(module)s - %(asctime)s - %(levelname)s @ %(funcName)s: %(message)s')
-    handler.setFormatter(formatter)
-    log.addHandler(handler)
+def get_logger(path='configs/logging.yaml'):
+    if os.path.exists(path):
+        with open(path, 'r') as f:
+            config = yaml.safe_load(f.read())
+        logging.config.dictConfig(config)
+        log = logging.getLogger('artemis_logger')
+        log.info('Loaded configuration from {}'.format(path))
+    else:
+        log = logging.getLogger('artemis_logger')
+        log.setLevel(logging.DEBUG)
+        handler = logging.handlers.SysLogHandler(
+            address=(SYSLOG_HOST, int(SYSLOG_PORT)))
+        formatter = logging.Formatter(
+            '%(module)s - %(asctime)s - %(levelname)s @ %(funcName)s: %(message)s')
+        handler.setFormatter(formatter)
+        log.addHandler(handler)
+        log.info('Loaded default configuration')
     return log
+
 
 # https://stackoverflow.com/questions/16136979/set-class-with-timed-auto-remove-of-elements
 class TimedSet(set):
@@ -41,13 +60,15 @@ class TimedSet(set):
             if time.time() < self.__table.get(item):
                 yield item
 
+
 def flatten(items, seqtypes=(list, tuple)):
     if not isinstance(items, seqtypes):
         return [items]
     for i in range(len(items)):
         while i < len(items) and isinstance(items[i], seqtypes):
-            items[i:i+1] = items[i]
+            items[i:i + 1] = items[i]
     return items
+
 
 class ArtemisError(Exception):
     def __init__(self, _type, _where):
@@ -58,6 +79,7 @@ class ArtemisError(Exception):
 
         # Call the base class constructor with the parameters it needs
         super().__init__(message)
+
 
 def exception_handler(log):
     def function_wrapper(f):

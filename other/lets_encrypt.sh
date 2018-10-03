a42785706120df3cb63dc5b7d4ad7f3561e71fae
vkotronis <biece89@gmail.com>
2018-10-03 21:51:51 +0300

    lets encrypt sample scripts and commented-in nginx configuration

---
+++ other/lets_encrypt.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -e
+
+sudo apt install -y software-properties-common
+sudo add-apt-repository -y ppa:certbot/certbot
+sudo apt-get update
+sudo apt install -y python-certbot-nginx
+
+# obtain certs for all the charthouse domains:
+sudo certbot --nginx certonly \
+     --keep --agree-tos \
+     --email <email_address> --no-eff-email \
+     --cert-name <domain_name> \
+     --domain <domain_name>

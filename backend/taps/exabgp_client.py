c04cc473c1d1124bf9deca08229f57023c49a83b
vkotronis <biece89@gmail.com>
2018-10-09 13:13:35 +0300

    updated exabgp client

--- backend/taps/exabgp_client.py
+++ backend/taps/exabgp_client.py
@@ -11,46 +11,47 @@ class ExaBGP():
         self.host = host
         self.prefixes = prefixes
 
-    def start_loop(self):
+    def start(self):
         with Connection(RABBITMQ_HOST) as connection:
-            self.start(connection)
+            self.connection = connection
+            self.exchange = Exchange(
+                'bgp-update',
+                channel=connection,
+                type='direct',
+                durable=False)
+            self.exchange.declare()
 
-    def start(self, connection):
-        self.connection = connection
-        self.exchange = Exchange(
-            'bgp-update',
-            channel=connection,
-            type='direct',
-            durable=False)
-        self.exchange.declare()
+            try:
+                sio = SocketIO('http://' + self.host, namespace=BaseNamespace, wait_for_connection=False)
 
-        with SocketIO('http://' + self.host, namespace=BaseNamespace, wait_for_connection=False) as sio:
+                def exabgp_msg(bgp_message):
+                    msg = {
+                        'type': bgp_message['type'],
+                        'communities': bgp_message.get('communities', []),
+                        'timestamp': bgp_message['timestamp'],
+                        'path': bgp_message.get('path', []),
+                        'service': 'exabgp|{}'.format(self.host),
+                        'prefix': bgp_message['prefix'],
+                        'peer_asn': int(bgp_message['peer_asn'])
+                    }
+                    if mformat_validator(msg):
+                        with Producer(connection) as producer:
+                            msgs = normalize_msg_path(msg)
+                            for msg in msgs:
+                                key_generator(msg)
+                                producer.publish(
+                                    msg,
+                                    exchange=self.exchange,
+                                    routing_key='update',
+                                    serializer='json'
+                                )
 
-            def exabgp_msg(bgp_message):
-                msg = {
-                    'type': bgp_message['type'],
-                    'communities': bgp_message.get('communities', []),
-                    'timestamp': bgp_message['timestamp'],
-                    'path': bgp_message.get('path', []),
-                    'service': 'exabgp|{}'.format(self.host),
-                    'prefix': bgp_message['prefix'],
-                    'peer_asn': int(bgp_message['peer_asn'])
-                }
-                if mformat_validator(msg):
-                    with Producer(connection) as producer:
-                        msgs = normalize_msg_path(msg)
-                        for msg in msgs:
-                            key_generator(msg)
-                            producer.publish(
-                                msg,
-                                exchange=self.exchange,
-                                routing_key='update',
-                                serializer='json'
-                            )
-
-            sio.on('exa_message', exabgp_msg)
-            sio.emit('exa_subscribe', {'prefixes': self.prefixes})
-            sio.wait()
+                sio.on('exa_message', exabgp_msg)
+                sio.emit('exa_subscribe', {'prefixes': self.prefixes})
+                sio.wait()
+            except KeyboardInterrupt:
+                sio.disconnect()
+                sio.wait()
 
 
 if __name__ == '__main__':
@@ -65,8 +66,6 @@ if __name__ == '__main__':
     prefixes = args.prefix.split(',')
     exa = ExaBGP(prefixes, args.host)
     try:
-        exa.start_loop()
-    except KeyboardInterrupt:
-        pass
+        exa.start()
     except BaseException:
         traceback.print_exc()

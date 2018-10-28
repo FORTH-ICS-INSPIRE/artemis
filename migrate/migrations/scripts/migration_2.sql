4f8322ac7966c22e3f0bfd0593608698e541cd77
Petros Gigis <gkigkis@ics.forth.gr>
2018-10-28 21:21:11 +0200

    update initial .sql file

--- migrate/migrations/scripts/migration_2.sql
+++ migrate/migrations/scripts/migration_2.sql
@@ -29,6 +29,6 @@ ALTER TABLE hijacks add CONSTRAINT possible_states CHECK (
 );
 
 
-CREATE OR REPLACE VIEW view_hijacks AS SELECT key, type, prefix, hijack_as, num_peers_seen, num_asns_inf, time_started, time_ended, time_last, mitigation_started, time_detected, timestamp_of_config, under_mitigation, resolved, active, ignored, configured_prefix, comment, withdrawn, peers_withdrawn FROM hijacks;
+CREATE OR REPLACE VIEW view_hijacks AS SELECT key, type, prefix, hijack_as, num_peers_seen, num_asns_inf, time_started, time_ended, time_last, mitigation_started, time_detected, timestamp_of_config, under_mitigation, resolved, active, ignored, configured_prefix, comment, seen, withdrawn, peers_withdrawn FROM hijacks;
 
 CREATE OR REPLACE VIEW view_bgpupdates AS SELECT prefix, origin_as, peer_asn, as_path, service, type, communities, timestamp, hijack_key, handled, matched_prefix, orig_path FROM bgp_updates;
--- other/db/data/tables.sql
+++ other/db/data/tables.sql
@@ -1,5 +1,26 @@
 CREATE EXTENSION IF NOT EXISTS timescaledb CASCADE;
 
+CREATE TABLE IF NOT EXISTS db_details (
+version BIGINT NOT NULL,
+upgraded_on TIMESTAMPTZ NOT NULL);
+
+CREATE UNIQUE INDEX db_details_one_row
+ON db_details((version IS NOT NULL));
+
+CREATE FUNCTION db_version_no_delete ()
+RETURNS trigger
+LANGUAGE plpgsql AS $f$
+BEGIN
+   RAISE EXCEPTION 'You may not delete the db_details!';
+END; $f$;
+
+CREATE TRIGGER db_details_no_delete
+BEFORE DELETE ON db_details
+FOR EACH ROW EXECUTE PROCEDURE db_version_no_delete();
+
+INSERT INTO db_details (version, upgraded_on) VALUES (2, now());
+
+
 CREATE TABLE IF NOT EXISTS bgp_updates (
     key VARCHAR ( 32 ) NOT NULL,
     prefix inet, origin_as BIGINT,
@@ -9,7 +30,7 @@ CREATE TABLE IF NOT EXISTS bgp_updates (
     type  VARCHAR ( 1 ),
     communities  json,
     timestamp TIMESTAMP  NOT NULL,
-    hijack_key VARCHAR ( 32 ),
+    hijack_key text[],
     handled   BOOLEAN,
     matched_prefix inet,
     orig_path json,
@@ -46,6 +67,7 @@ CREATE TABLE IF NOT EXISTS hijacks (
     configured_prefix  inet,
     timestamp_of_config TIMESTAMP,
     comment text,
+    seen BOOLEAN DEFAULT FALSE,
     PRIMARY KEY(time_detected, key),
     UNIQUE(time_detected, key),
     CONSTRAINT possible_states CHECK (
@@ -79,7 +101,7 @@ CREATE TABLE IF NOT EXISTS configs (
 
 CREATE OR REPLACE VIEW view_configs AS SELECT time_modified FROM configs;
 
-CREATE OR REPLACE VIEW view_hijacks AS SELECT key,type, prefix, hijack_as, num_peers_seen, num_asns_inf, time_started, time_ended, time_last, mitigation_started, time_detected, timestamp_of_config, under_mitigation, resolved, active, ignored, configured_prefix, comment FROM hijacks;
+CREATE OR REPLACE VIEW view_hijacks AS SELECT key, type, prefix, hijack_as, num_peers_seen, num_asns_inf, time_started, time_ended, time_last, mitigation_started, time_detected, timestamp_of_config, under_mitigation, resolved, active, ignored, configured_prefix, comment, seen, withdrawn, peers_withdrawn FROM hijacks;
 
 CREATE OR REPLACE VIEW view_bgpupdates AS SELECT prefix, origin_as, peer_asn, as_path, service, type, communities, timestamp, hijack_key, handled, matched_prefix, orig_path FROM bgp_updates;
 

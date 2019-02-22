4aaae6b5106a55f561286266eb86d1cad9e7dcc6
slowr <jim.mavrommatis@gmail.com>
2019-02-22 08:36:21 -0800

    tester restructure

--- backend/testing/tester.py
+++ backend/testing/tester.py
@@ -65,6 +65,109 @@ class Tester():
         self.send_cnt = 0
         self.expected_messages = 0
 
+    @staticmethod
+    def redis_key(prefix, hijack_as, _type):
+        assert isinstance(prefix, str)
+        assert isinstance(hijack_as, int)
+        assert isinstance(_type, str)
+        return hashlib.shake_128(pickle.dumps([prefix, hijack_as,
+                                               _type])).hexdigest(16)
+
+    @staticmethod
+    def waitExchange(exchange, channel):
+        '''
+        Wait passively until the exchange is declared.
+        '''
+        while True:
+            try:
+                exchange.declare(passive=True, channel=channel)
+                break
+            except Exception:
+                time.sleep(1)
+
+    def waitProcess(self, mod, target):
+        state = self.supervisor.supervisor.getProcessInfo(mod)['state']
+        while state != target:
+            time.sleep(0.5)
+            state = self.supervisor.supervisor.getProcessInfo(mod)['state']
+
+    def validate_message(self, body, message):
+        '''
+        Callback method for message validation from the queues.
+        '''
+        print('\t- Test \"{}\" - Receiving Batch #{} - Type {} - Remaining {}'.
+              format(self.curr_test, self.curr_idx,
+                     message.delivery_info['routing_key'],
+                     self.expected_messages - 1))
+        if isinstance(body, dict):
+            event = body
+        else:
+            event = json.loads(body)
+        # logging.debug(event)
+
+        # distinguish between type of messages
+        if message.delivery_info['routing_key'] == 'update-update':
+            expected = self.messages[
+                self.curr_idx]['detection_update_response']
+            assert self.redis.exists(
+                event['key']), 'Monitor key not found in Redis'
+        elif message.delivery_info['routing_key'] == 'update':
+            expected = self.messages[
+                self.curr_idx]['detection_hijack_response']
+            redis_hijack_key = Tester.redis_key(
+                event['prefix'], event['hijack_as'], event['type'])
+            assert self.redis.exists(
+                redis_hijack_key), 'Hijack key not found in Redis'
+        elif message.delivery_info['routing_key'] == 'hijack-update':
+            expected = self.messages[self.curr_idx]['database_hijack_response']
+            if event['active']:
+                assert self.redis.sismember(
+                    'persistent-keys',
+                    event['key']), 'Persistent key not found in Redis'
+            else:
+                assert not self.redis.sismember(
+                    'persistent-keys', event['key']
+                ), 'Persistent key found in Redis but should have been removed.'
+
+        # compare expected message with received one. exit on
+        # mismatch.
+        for key in set(event.keys()).intersection(expected.keys()):
+            if 'time' in key:
+                expected[key] += self.time_now
+            assert (
+                event[key] == expected[key]
+                or (isinstance(event[key], (list, set))
+                    and set(event[key]) == set(expected[key]))
+            ), ('Test \"{}\" - Batch #{} - Type {}: Unexpected value for key \"{}\". Received: {}, Expected: {}'
+                .format(self.curr_test, self.curr_idx,
+                        message.delivery_info['routing_key'], key, event[key],
+                        expected[key]))
+
+        self.expected_messages -= 1
+        if self.expected_messages <= 0:
+            self.curr_idx += 1
+        message.ack()
+
+    def send_next_message(self, conn):
+        '''
+        Publish next custom BGP update on the bgp-updates exchange.
+        '''
+        with conn.Producer() as producer:
+            self.expected_messages = len(self.messages[self.curr_idx]) - 1
+            print('Publishing #{}'.format(self.curr_idx))
+            # logging.debug(messages[curr_idx]['send'])
+
+            # offset to account for "real-time" tests
+            for key in self.messages[self.curr_idx]['send']:
+                if 'time' in key:
+                    self.messages[self.curr_idx]['send'][key] += self.time_now
+
+            producer.publish(
+                self.messages[self.curr_idx]['send'],
+                exchange=self.update_exchange,
+                routing_key='update',
+                serializer='json')
+
     def test(self):
         '''
         Loads a test file that includes crafted bgp updates as input and expected messages as output.
@@ -73,61 +176,52 @@ class Tester():
         RABBITMQ_HOST = os.getenv('RABBITMQ_HOST')
 
         # exchanges
-        update_exchange = Exchange(
+        self.update_exchange = Exchange(
             'bgp-update', type='direct', durable=False, delivery_mode=1)
 
-        hijack_exchange = Exchange(
+        self.hijack_exchange = Exchange(
             'hijack-update', type='direct', durable=False, delivery_mode=1)
 
-        pg_amq_bridge = Exchange(
+        self.pg_amq_bridge = Exchange(
             'amq.direct', type='direct', durable=True, delivery_mode=1)
 
         # queues
-        update_queue = Queue(
+        self.update_queue = Queue(
             'detection-testing',
-            exchange=pg_amq_bridge,
+            exchange=self.pg_amq_bridge,
             routing_key='update-update',
             durable=False,
             auto_delete=True,
             max_priority=1,
             consumer_arguments={'x-priority': 1})
 
-        hijack_queue = Queue(
+        self.hijack_queue = Queue(
             'hijack-testing',
-            exchange=hijack_exchange,
+            exchange=self.hijack_exchange,
             routing_key='update',
             durable=False,
             auto_delete=True,
             max_priority=1,
             consumer_arguments={'x-priority': 1})
 
-        hijack_db_queue = Queue(
+        self.hijack_db_queue = Queue(
             'hijack-db-testing',
-            exchange=pg_amq_bridge,
+            exchange=self.pg_amq_bridge,
             routing_key='hijack-update',
             durable=False,
             auto_delete=True,
             max_priority=1,
             consumer_arguments={'x-priority': 1})
 
-        def waitExchange(exchange, channel):
-            '''
-            Wait passively until the exchange is declared.
-            '''
-            while True:
-                try:
-                    exchange.declare(passive=True, channel=channel)
-                    break
-                except Exception:
-                    time.sleep(1)
-
         with Connection(RABBITMQ_HOST) as connection:
             print('Waiting for pg_amq exchange..')
-            waitExchange(pg_amq_bridge, connection.default_channel)
+            Tester.waitExchange(self.pg_amq_bridge, connection.default_channel)
             print('Waiting for hijack exchange..')
-            waitExchange(hijack_exchange, connection.default_channel)
+            Tester.waitExchange(self.hijack_exchange,
+                                connection.default_channel)
             print('Waiting for update exchange..')
-            waitExchange(update_exchange, connection.default_channel)
+            Tester.waitExchange(self.update_exchange,
+                                connection.default_channel)
 
             # query database for the states of the processes
             db_con = self.getDbConnection()
@@ -148,118 +242,30 @@ class Tester():
                 self.clear()
 
                 self.curr_test = testfile
-                messages = {}
+                self.messages = {}
                 # load test
                 with open('testfiles/{}'.format(testfile), 'r') as f:
-                    messages = json.load(f)
+                    self.messages = json.load(f)
 
-                send_len = len(messages)
-
-                def redis_key(prefix, hijack_as, _type):
-                    assert isinstance(prefix, str)
-                    assert isinstance(hijack_as, int)
-                    assert isinstance(_type, str)
-                    return hashlib.shake_128(
-                        pickle.dumps([prefix, hijack_as, _type])).hexdigest(16)
-
-                def validate_message(body, message):
-                    '''
-                    Callback method for message validation from the queues.
-                    '''
-                    print(
-                        '\t- Test \"{}\" - Receiving Batch #{} - Type {} - Remaining {}'
-                        .format(self.curr_test, self.curr_idx,
-                                message.delivery_info['routing_key'],
-                                self.expected_messages - 1))
-                    if isinstance(body, dict):
-                        event = body
-                    else:
-                        event = json.loads(body)
-                    # logging.debug(event)
-
-                    # distinguish between type of messages
-                    if message.delivery_info['routing_key'] == 'update-update':
-                        expected = messages[
-                            self.curr_idx]['detection_update_response']
-                        assert self.redis.exists(
-                            event['key']), 'Monitor key not found in Redis'
-                    elif message.delivery_info['routing_key'] == 'update':
-                        expected = messages[
-                            self.curr_idx]['detection_hijack_response']
-                        redis_hijack_key = redis_key(
-                            event['prefix'], event['hijack_as'], event['type'])
-                        assert self.redis.exists(
-                            redis_hijack_key), 'Hijack key not found in Redis'
-                    elif message.delivery_info[
-                            'routing_key'] == 'hijack-update':
-                        expected = messages[
-                            self.curr_idx]['database_hijack_response']
-                        if event['active']:
-                            assert self.redis.sismember(
-                                'persistent-keys', event['key']
-                            ), 'Persistent key not found in Redis'
-                        else:
-                            assert not self.redis.sismember(
-                                'persistent-keys', event['key']
-                            ), 'Persistent key found in Redis but should have been removed.'
-
-                    # compare expected message with received one. exit on
-                    # mismatch.
-                    for key in set(event.keys()).intersection(expected.keys()):
-                        if 'time' in key:
-                            expected[key] += self.time_now
-                        assert (
-                            event[key] == expected[key]
-                            or (isinstance(event[key], (list, set))
-                                and set(event[key]) == set(expected[key]))
-                        ), ('Test \"{}\" - Batch #{} - Type {}: Unexpected value for key \"{}\". Received: {}, Expected: {}'
-                            .format(self.curr_test, self.curr_idx,
-                                    message.delivery_info['routing_key'], key,
-                                    event[key], expected[key]))
-
-                    self.expected_messages -= 1
-                    if self.expected_messages <= 0:
-                        self.curr_idx += 1
-                    message.ack()
-
-                def send_next_message(conn):
-                    '''
-                    Publish next custom BGP update on the bgp-updates exchange.
-                    '''
-                    with conn.Producer() as producer:
-                        self.expected_messages = len(
-                            messages[self.curr_idx]) - 1
-                        print('Publishing #{}'.format(self.curr_idx))
-                        # logging.debug(messages[curr_idx]['send'])
-
-                        # offset to account for "real-time" tests
-                        for key in messages[self.curr_idx]['send']:
-                            if 'time' in key:
-                                messages[self.curr_idx]['send'][
-                                    key] += self.time_now
-
-                        producer.publish(
-                            messages[self.curr_idx]['send'],
-                            exchange=update_exchange,
-                            routing_key='update',
-                            serializer='json')
+                send_len = len(self.messages)
 
                 with nested(
                         connection.Consumer(
-                            hijack_queue,
-                            callbacks=[validate_message],
+                            self.hijack_queue,
+                            callbacks=[self.validate_message],
                             accept=['pickle']),
                         connection.Consumer(
-                            update_queue,
-                            callbacks=[validate_message],
+                            self.update_queue,
+                            callbacks=[self.validate_message],
                         ),
                         connection.Consumer(
-                            hijack_db_queue, callbacks=[validate_message])):
+                            self.hijack_db_queue,
+                            callbacks=[self.validate_message])):
                     send_cnt = 0
                     # send and validate all messages in the messages.json file
                     while send_cnt < send_len:
                         self.curr_idx = send_cnt
-                        send_next_message(connection)
+                        self.send_next_message(connection)
                         send_cnt += 1
                         # sleep until we receive all expected messages
                         while self.curr_idx != send_cnt:
@@ -272,27 +278,21 @@ class Tester():
 
             connection.close()
 
-        def waitProcess(mod, target):
-            state = self.supervisor.supervisor.getProcessInfo(mod)['state']
-            while state != target:
-                time.sleep(0.5)
-                state = self.supervisor.supervisor.getProcessInfo(mod)['state']
-
         self.supervisor.supervisor.stopAllProcesses()
 
-        waitProcess('listener', 0)  # 0 STOPPED
-        waitProcess('clock', 0)  # 0 STOPPED
-        waitProcess('detection', 0)  # 0 STOPPED
-        waitProcess('mitigation', 0)  # 0 STOPPED
-        waitProcess('configuration', 0)  # 0 STOPPED
-        waitProcess('database', 0)  # 0 STOPPED
-        waitProcess('observer', 0)  # 0 STOPPED
-        waitProcess('monitor', 0)  # 0 STOPPED
+        self.waitProcess('listener', 0)  # 0 STOPPED
+        self.waitProcess('clock', 0)  # 0 STOPPED
+        self.waitProcess('detection', 0)  # 0 STOPPED
+        self.waitProcess('mitigation', 0)  # 0 STOPPED
+        self.waitProcess('configuration', 0)  # 0 STOPPED
+        self.waitProcess('database', 0)  # 0 STOPPED
+        self.waitProcess('observer', 0)  # 0 STOPPED
+        self.waitProcess('monitor', 0)  # 0 STOPPED
 
         self.supervisor.supervisor.startProcess('coveralls')
 
-        waitProcess('coveralls', 20)  # 20 RUNNING
-        waitProcess('coveralls', 100)  # 0 EXITED
+        self.waitProcess('coveralls', 20)  # 20 RUNNING
+        self.waitProcess('coveralls', 100)  # 0 EXITED
 
 
 if __name__ == "__main__":

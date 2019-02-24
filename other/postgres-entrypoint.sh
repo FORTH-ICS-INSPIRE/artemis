a181a91ae3e4553b2a0cac5767d3f3948c6d4b56
Dimitrios Mavrommatis <jim.mavrommatis+github@gmail.com>
2019-02-24 20:47:34 +0200

    Auto-clean no-hijack BGP updates (Closes #33) (#103)
    * Enabled by default to clean unwanted updates after 1 hour and backup daily
    
    * Added env. variables to set backup and autoclean
    
    * updated default .env
    
    * added +x access

---
+++ other/postgres-entrypoint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+if [[ "${DB_BACKUP}" == "true" ]]; then
+    cat > /etc/periodic/daily/backup <<EOF
+#!/bin/sh
+pg_dump -d $POSTGRES_DB -U $POSTGRES_USER -F t -f /tmp/db.tar > /tmp/db.log 2>&1
+EOF
+    chmod +x /etc/periodic/daily/backup
+fi
+
+re='^[0-9]+$'
+if [[ $DB_AUTOCLEAN =~ $re ]]; then
+    cat > /etc/periodic/hourly/cleanup <<EOF
+#!/bin/sh
+psql -d $POSTGRES_DB -U $POSTGRES_USER -c "DELETE FROM bgp_updates WHERE timestamp < NOW() - interval '${DB_AUTOCLEAN} hours' AND hijack_key=ARRAY[]::text[];"
+EOF
+    chmod +x /etc/periodic/hourly/cleanup
+fi
+
+crond && docker-entrypoint.sh postgres

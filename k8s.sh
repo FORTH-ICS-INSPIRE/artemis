9af9f139a2b233de177e6b01e6f622b5ed129d68
slowr <jim.mavrommatis@gmail.com>
2019-03-07 16:19:00 +0000

    changed names to battle kompose's misbehavior with _ and -
    Signed-off-by: slowr <jim.mavrommatis@gmail.com>

--- k8s.sh
+++ k8s.sh
@@ -4,7 +4,6 @@ if [[ -f .env && -f docker-compose.yaml ]]; then
     cwd=$(pwd)
     sed -i "/path:.*nginx.conf/a \            type: File" k8s.yaml
     sed -i "/path:.*enabled-plugins/a \            type: File" k8s.yaml
-    sed -i "s/enabled-plugins/enabled_plugins/g" k8s.yaml
     sed -i "/path:.*postgres-entrypoint.sh/a \            type: File" k8s.yaml
     sed -i "/path:.*init.sql/a \            type: File" k8s.yaml
     sed -i "/path:.*wait-for/a \            type: File" k8s.yaml

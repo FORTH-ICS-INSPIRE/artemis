88ae21251886fc82747697c2679a66d8cf1155ad
slowr <jim.mavrommatis@gmail.com>
2019-03-12 13:49:23 +0000

    configmap, secrets and volumes
    Signed-off-by: slowr <jim.mavrommatis@gmail.com>

---
+++ helm/files/configmaps/init.sql
@@ -0,0 +1,22 @@
+-- some setting to make the output less verbose
+\set QUIET on
+\set ON_ERROR_STOP on
+set client_min_messages to warning;
+
+-- load some variables from the env
+\setenv base_dir :DIR
+\set base_dir `if [ $base_dir != ":"DIR ]; then echo $base_dir; else echo "/docker-entrypoint-initdb.d"; fi`
+
+
+\echo # Loading database definition
+begin;
+
+\echo # Loading dependencies
+-- functions for sending messages to RabbitMQ entities
+\ir libs/rabbitmq/schema.sql
+
+\echo # Loading application definitions
+\ir data/tables.sql
+
+commit;
+\echo # ==========================================

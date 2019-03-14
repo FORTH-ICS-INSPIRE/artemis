9fdb92e458a632d4b5bd26c33efa8caed1394ee6
slowr <jim.mavrommatis@gmail.com>
2019-03-14 15:07:32 +0000

    symbolic links for used files
    Signed-off-by: slowr <jim.mavrommatis@gmail.com>

--- artemis-chart/files/configmaps/restore.sql
+++ artemis-chart/files/configmaps/restore.sql
@@ -1,6 +1,1 @@
-ALTER DATABASE artemis_db SET timescaledb.restoring='on';
--- execute the restore (or from a shell)
-\! pg_restore -Ft -U artemis_user -d artemis_db /tmp/db.tar
--- connect to the restored db
-\c artemis_db
-ALTER DATABASE artemis_db SET timescaledb.restoring='off';
+../../../other/db/data/restore.sql

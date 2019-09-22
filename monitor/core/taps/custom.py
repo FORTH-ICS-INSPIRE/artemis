f3bf9891c4efaa27f5e495344239138e564a6d43
slowr <jim.mavrommatis@gmail.com>
2019-09-22 12:17:43 -0600

    added a recv script to check receive rates

--- monitor/core/taps/custom.py
+++ monitor/core/taps/custom.py
@@ -9,94 +9,58 @@ from kombu import uuid
 from utils import get_logger
 from utils import RABBITMQ_URI
 
+import sys
+
 log = get_logger()
 
-recv_cnt = 0
-send_cnt = 0
+if len(sys.argv) == 2:
+    LIMIT_UPDATES = int(sys.argv[1])
+else:
+    LIMIT_UPDATES = 65536
 
+if LIMIT_UPDATES > 65536:
+    print('Cannot support more that 65536 updates at the moment')
+    sys.exit()
 
-def run():
-    def sender(k):
-        global send_cnt
-        send_cnt = 0
-        msg_ = {
-            "timestamp": 1,
-            "orig_path": [],
-            "communities": [],
-            "service": "a",
-            "type": "A",
-            "path": [8, 3, 2, 1, ord(k)],
-            "prefix": "10.0.0.0/24",
-            "peer_asn": 8,
-        }
-        with Connection(RABBITMQ_URI) as connection:
-            exchange = Exchange(
-                "bgp-update", channel=connection, type="direct", durable=False
-            )
-            exchange.declare()
-            with Producer(connection) as producer:
-                for x in range(1, 256):
-                    for y in range(1, 256):
-                        for z in range(1, 256):
-                            msg_["timestamp"] = x * 1000000 + y * 1000 + z
-                            msg_["key"] = "{}-{}-{}-{}".format(k, x, y, z)
-                            msg_["prefix"] = "{}.{}.{}.0/24".format(x, y, z)
-                            producer.publish(
-                                msg_,
-                                exchange=exchange,
-                                routing_key="update",
-                                serializer="json",
-                            )
-                            send_cnt += 1
-                            print("Total sent {} from {}".format(send_cnt, k))
-
-    def receiver():
-        def on_response():
-            global recv_cnt
-            recv_cnt += 1
-            print("Total received {}".format(recv_cnt))
-
-        with Connection(RABBITMQ_URI) as connection:
-            hijack_exchange = Exchange(
-                "hijack-update",
-                channel=connection,
-                type="direct",
-                durable=False,
-                delivery_mode=1,
-            )
-            hijack_exchange.declare()
-            hijack_queue = Queue(
-                "db-hijack-update-{}".format(uuid()),
-                exchange=hijack_exchange,
-                routing_key="1",
-                durable=False,
-                auto_delete=True,
-                max_priority=1,
-                consumer_arguments={"x-priority": 1},
-            )
-            with Consumer(
-                connection, on_message=on_response, queues=[hijack_queue], no_ack=True
-            ):
-                while True:
-                    connection.drain_events()
-
-    send_threads = []
-    for i in range(1):
-        send_threads.append(threading.Thread(target=sender, args=(chr(i + 97),)))
-    recv_thread = threading.Thread(target=receiver, args=())
-
-    for t in send_threads:
-        t.start()
-    recv_thread.start()
-
-    for t in send_threads:
-        t.join()
+def sender():
+    send_cnt = 0
+    msg_ = {
+        "orig_path": [],
+        "communities": [],
+        "service": "a",
+        "type": "A",
+        "path": [8, 4, 3, 2, 1],
+        "peer_asn": 8,
+    }
+    with Connection(RABBITMQ_URI) as connection:
+        exchange = Exchange(
+            "bgp-update", channel=connection, type="direct", durable=False
+        )
+        exchange.declare()
+        with Producer(connection) as producer:
+            for x in range(0, 256):
+                if send_cnt // LIMIT_UPDATES > 0:
+                    break
+                for y in range(0, 256):
+                    if send_cnt // LIMIT_UPDATES > 0:
+                        break
+                    msg_["timestamp"] = x * 1000 + y
+                    msg_["key"] = "{}-{}".format(x, y)
+                    msg_["prefix"] = "10.{}.{}.0/24".format(x, y)
+                    producer.publish(
+                        msg_,
+                        exchange=exchange,
+                        routing_key="update",
+                        serializer="json",
+                    )
+                    send_cnt += 1
+                    print("Total send {}\r".format(send_cnt))
 
 
 if __name__ == "__main__":
     try:
-        run()
-    except Exception:
-        log.exception("exception")
+        sender()
+    except Exception as e:
+        print("Exception: {}".format(e))
     except KeyboardInterrupt:
         pass
---
+++ monitor/core/taps/recv.py
@@ -0,0 +1,68 @@
+import threading
+
+from kombu import Connection
+from kombu import Consumer
+from kombu import Exchange
+from kombu import Producer
+from kombu import Queue
+from kombu import uuid
+from utils import RABBITMQ_URI
+
+from tqdm import tqdm
+
+import sys
+
+
+if len(sys.argv) == 4:
+    EXCHANGE_NAME = sys.argv[1]
+    ROUTING_KEY = sys.argv[2]
+    LIMIT_UPDATES = int(sys.argv[3])
+elif len(sys.argv) == 3:
+    EXCHANGE_NAME = sys.argv[1]
+    ROUTING_KEY = sys.argv[2]
+    LIMIT_UPDATES = 65536
+else:
+    print("usage: python {} exchange_name routing_key update_num")
+    sys.exit()
+
+if LIMIT_UPDATES > 65536:
+    print("Cannot support more that 65536 updates at the moment")
+    sys.exit()
+
+
+def bind_and_wait(connection, queue):
+    queue.declare(channel=connection.default_channel)
+    bind_queue = queue.bind(connection.default_channel)
+    for i in tqdm(range(LIMIT_UPDATES)):
+        while True:
+            if bind_queue.get():
+                break
+
+def receiver():
+    with Connection(RABBITMQ_URI) as connection:
+        exchange = Exchange(
+            EXCHANGE_NAME,
+            channel=connection,
+            type="direct",
+            durable=False,
+            delivery_mode=1,
+        )
+        exchange.declare()
+        queue = Queue(
+            "{}".format(uuid()),
+            exchange=exchange,
+            routing_key=ROUTING_KEY,
+            durable=False,
+            auto_delete=True,
+            max_priority=1,
+            consumer_arguments={"x-priority": 1},
+            channel=connection.default_channel,
+        )
+        bind_and_wait(connection, queue) 
+
+
+if __name__ == "__main__":
+    try:
+        receiver()
+    except KeyboardInterrupt:
+        pass

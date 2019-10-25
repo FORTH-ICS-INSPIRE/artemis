b19176adc0349107a14c498069056adbb3613da1
vkotronis <biece89@gmail.com>
2019-10-25 14:08:33 +0300

    sending autoconf updates in autoconf tester

--- autoconf-tester/tester.py
+++ autoconf-tester/tester.py
@@ -1,15 +1,11 @@
 import os
-import sys
 import time
-from xmlrpc.client import ServerProxy
 
-# from multiprocessing import Process
-
-# from kombu import Connection
-# from kombu import Exchange
-# from kombu import Producer
-# from kombu import Queue
-# from kombu import uuid
+from kombu import Connection
+from kombu import Consumer
+from kombu import Producer
+from kombu import Queue
+from kombu import uuid
 
 RABBITMQ_USER = os.getenv("RABBITMQ_USER", "guest")
 RABBITMQ_PASS = os.getenv("RABBITMQ_PASS", "guest")
@@ -26,32 +22,104 @@ BACKEND_SUPERVISOR_URI = "http://{}:{}/RPC2".format(
 )
 
 
-def wait():
-    ctx = ServerProxy(BACKEND_SUPERVISOR_URI)
+class AutoconfTester:
+    def __init__(self):
+        self.time_now = int(time.time())
+        self.autoconf_goahead = False
 
-    try:
-        state = ctx.supervisor.getProcessInfo("detection")["state"]
-        while state == 10:
-            print("[!] Waiting for Detection")
-            time.sleep(0.5)
-            state = ctx.supervisor.getProcessInfo("detection")["state"]
-    except Exception as e:
-        print(e)
-        sys.exit(-1)
-    print("[!] Detection is running")
+    def handle_autoconf_update_goahead_reply(self, message):
+        self.autoconf_goahead = True
 
+    def send(self, msg):
+        with Connection(RABBITMQ_URI) as connection:
+            correlation_id = uuid()
+            callback_queue = Queue(
+                uuid(),
+                durable=False,
+                auto_delete=True,
+                max_priority=4,
+                consumer_arguments={"x-priority": 4},
+            )
+            with Producer(connection) as producer:
 
-def send():
-    pass
-
-
-def receive(exchange_name, routing_key):
-    pass
+                producer.publish(
+                    msg,
+                    exchange="",
+                    routing_key="conf-autoconf-update-queue",
+                    reply_to=callback_queue.name,
+                    correlation_id=correlation_id,
+                    retry=True,
+                    declare=[
+                        Queue(
+                            "conf-autoconf-update-queue",
+                            durable=False,
+                            max_priority=4,
+                            consumer_arguments={"x-priority": 4},
+                        ),
+                        callback_queue,
+                    ],
+                    priority=4,
+                    serializer="json",
+                )
+            with Consumer(
+                connection,
+                on_message=self.handle_autoconf_update_goahead_reply,
+                queues=[callback_queue],
+                no_ack=True,
+            ):
+                while not self.autoconf_goahead:
+                    connection.drain_events()
 
 
 if __name__ == "__main__":
     print("[+] Starting")
-    wait()
-    # TODO
-    pass
+    autoconf_tester = AutoconfTester()
+
+    print("[+] Sending announcement with origin only")
+    announcement_origin = {
+        "key": 1,
+        "type": "A",
+        "timestamp": autoconf_tester.time_now + 1,
+        "path": [1],
+        "service": "test-autoconf",
+        "communities": [],
+        "prefix": "192.168.0.0/16",
+        "peer_asn": 1,
+    }
+    autoconf_tester.send(announcement_origin)
+    print("[+] Announcement with origin only sent")
+
+    # TODO handle conf change check!
+
+    print("[+] Sending announcement with origin and neighbors")
+    announcement_origin_with_neighbors = {
+        "key": 2,
+        "type": "A",
+        "timestamp": autoconf_tester.time_now + 2,
+        "path": [1],
+        "service": "test-autoconf",
+        "communities": [{"asn": 1, "value": 2}, {"asn": 1, "value": 3}],
+        "prefix": "192.168.0.0/16",
+        "peer_asn": 1,
+    }
+    autoconf_tester.send(announcement_origin_with_neighbors)
+    print("[+] Announcement with origin and neighbors sent")
+
+    # TODO handle conf change check!
+
+    print("[+] Sending prefix withdrawal")
+    withdrawal = {
+        "key": 3,
+        "type": "W",
+        "timestamp": autoconf_tester.time_now + 2,
+        "path": [],
+        "service": "test-autoconf",
+        "communities": [],
+        "prefix": "192.168.0.0/16",
+        "peer_asn": 1,
+    }
+    print("[+] Withdrawal sent")
+
+    # TODO handle conf change check!
+
     print("[+] Exiting")

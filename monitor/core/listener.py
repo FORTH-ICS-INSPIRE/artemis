71bb5077c15215cf3d76e1fc9406cc12fe24aae4
slowr <jim.mavrommatis@gmail.com>
2019-11-06 22:33:48 +0100

    Revert back listeners because of weird behaviour

--- monitor/core/listener.py
+++ monitor/core/listener.py
@@ -1,8 +1,9 @@
 import os
 import sys
+import time
 
+import psycopg2
 from supervisor.childutils import listener
-from utils.tool import DB
 
 
 def write_stdout(s):
@@ -22,22 +23,30 @@ query = (
 )
 
 
-def run():
-    _db_name = os.getenv("DB_NAME", "artemis_db")
-    _user = os.getenv("DB_USER", "artemis_user")
-    _host = os.getenv("DB_HOST", "postgres")
-    _port = os.getenv("DB_PORT", 5432)
-    _pass = os.getenv("DB_PASS", "Art3m1s")
+def create_connect_db():
+    _db_conn = None
+    while not _db_conn:
+        try:
+            _db_name = os.getenv("DB_NAME", "artemis_db")
+            _user = os.getenv("DB_USER", "artemis_user")
+            _host = os.getenv("DB_HOST", "postgres")
+            _port = os.getenv("DB_PORT", 5432)
+            _password = os.getenv("DB_PASS", "Art3m1s")
 
-    db = DB(
-        user=_user,
-        password=_pass,
-        host=_host,
-        port=_port,
-        database=_db_name,
-        reconnect=True,
-    )
+            _db_conn = psycopg2.connect(
+                dbname=_db_name, user=_user, host=_host, port=_port, password=_password
+            )
+        except BaseException as e:
+            time.sleep(1)
+            write_stderr("Db connection exception: {}".format(e))
+
+    return _db_conn
+
+
+def run():
     while True:
+        db_conn = create_connect_db()
+        db_cursor = db_conn.cursor()
         headers, body = listener.wait(sys.stdin, sys.stdout)
         body = dict([pair.split(":") for pair in body.split(" ")])
         # write_stderr('{} | {}'.format(headers, body))
@@ -46,8 +55,12 @@ def run():
             process = body["processname"]
             if process != "listener":
                 new_state = headers["eventname"] == "PROCESS_STATE_RUNNING"
-                write_stderr("{} -> {}".format(process, new_state))
-                db.execute(query, (process, new_state))
+                try:
+                    write_stderr("{} -> {}".format(process, new_state))
+                    db_cursor.execute(query, (process, new_state))
+                    db_conn.commit()
+                except Exception:
+                    db_conn.rollback()
 
         # acknowledge the event
         write_stdout("RESULT 2\nOK")

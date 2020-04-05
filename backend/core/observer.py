7e9e6505bef169f2ff3eaab23a2ebffa772a966a
vkotronis <biece89@gmail.com>
2020-04-05 19:40:55 +0300

    first try with autoconf observer crash bug

--- backend/core/observer.py
+++ backend/core/observer.py
@@ -2,17 +2,28 @@ import difflib
 import signal
 import time
 
+import ujson as json
 from kombu import Connection
 from kombu import Consumer
 from kombu import Producer
 from kombu import Queue
+from kombu import serialization
 from kombu import uuid
 from utils import get_logger
 from utils import RABBITMQ_URI
 from watchdog.events import FileSystemEventHandler
 from watchdog.observers import Observer as WatchObserver
 
+
 log = get_logger()
+
+serialization.register(
+    "ujson",
+    json.dumps,
+    json.loads,
+    content_type="application/x-ujson",
+    content_encoding="utf-8",
+)
 
 
 class Observer:
@@ -51,6 +62,8 @@ class Observer:
         def __init__(self, d, fn, connection):
             super().__init__()
             self.connection = connection
+            self.response = None
+            self.correlation_id = None
             self.path = "{}/{}".format(d, fn)
             with open(self.path, "r") as f:
                 self.content = f.readlines()
@@ -95,9 +108,15 @@ class Observer:
                         self.connection,
                         on_message=self.on_response,
                         queues=[callback_queue],
+                        accept=["ujson"],
                     ):
-                        while not self.response:
+                        while self.response is None:
                             self.connection.drain_events()
+                    log.info(
+                        "Received response from configuration module with status '{}'".format(
+                            str(self.response["status"])
+                        )
+                    )
 
                     if self.response["status"] == "accepted":
                         text = "new configuration accepted:\n{}".format(changes)

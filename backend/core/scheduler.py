072c29e8cfc821712141543a115fa7d8bd039762
vkotronis <biece89@gmail.com>
2020-05-04 20:52:50 +0300

    using scheduler/clock to send periodic messages to database for auto-ignore

--- backend/core/scheduler.py
+++ backend/core/scheduler.py
@@ -4,6 +4,7 @@ import time
 from kombu import Connection
 from kombu import Exchange
 from kombu import Producer
+from utils import AUTO_IGNORE_INTERVAL
 from utils import get_logger
 from utils import RABBITMQ_URI
 
@@ -27,9 +28,12 @@ class Scheduler:
 
     class Worker:
         def __init__(self, connection):
+            self.clock = 0
             self.connection = connection
             # Time in secs to gather entries to perform a bulk operation
-            self.time_to_wait = float(os.getenv("BULK_TIMER", 1))
+            self.time_to_wait_bulk = float(os.getenv("BULK_TIMER", 1))
+            # Time in secs to check for hijack alerts to be auto-ignored
+            self.time_to_wait_auto_ignore = AUTO_IGNORE_INTERVAL
 
             self.db_clock_exchange = Exchange(
                 "db-clock",
@@ -45,7 +49,8 @@ class Scheduler:
         def _db_clock_send(self):
             with Producer(self.connection) as producer:
                 while True:
-                    time.sleep(self.time_to_wait)
+                    time.sleep(self.time_to_wait_bulk)
+                    self.clock += self.time_to_wait_bulk
                     producer.publish(
                         {"op": "bulk_operation"},
                         exchange=self.db_clock_exchange,
@@ -54,6 +59,16 @@ class Scheduler:
                         priority=3,
                         serializer="ujson",
                     )
+                    if self.clock >= self.time_to_wait_auto_ignore:
+                        producer.publish(
+                            {"op": "auto_ignore_check"},
+                            exchange=self.db_clock_exchange,
+                            routing_key="pulse",
+                            retry=True,
+                            priority=2,
+                            serializer="ujson",
+                        )
+                        self.clock = 0
 
 
 def run():

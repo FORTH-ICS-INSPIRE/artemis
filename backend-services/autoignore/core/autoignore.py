3b1d87e12b7dbeb148976c907fe8429525e7cd34
Vasileios Kotronis <biece89@gmail.com>
2020-11-24 18:11:46 +0200

    initial autoignore container

---
+++ backend-services/autoignore/core/autoignore.py
@@ -0,0 +1,502 @@
+import multiprocessing as mp
+import os
+import threading
+import time
+from typing import Dict
+from typing import NoReturn
+
+import requests
+import ujson as json
+from artemis_utils import DB_HOST
+from artemis_utils import DB_NAME
+from artemis_utils import DB_PASS
+from artemis_utils import DB_PORT
+from artemis_utils import DB_USER
+from artemis_utils import get_logger
+from artemis_utils import RABBITMQ_URI
+from artemis_utils.db_util import DB
+from artemis_utils.rabbitmq_util import create_exchange
+from artemis_utils.rabbitmq_util import create_queue
+from kombu import Connection
+from kombu import Producer
+from kombu import uuid
+from kombu.mixins import ConsumerProducerMixin
+from tornado.ioloop import IOLoop
+from tornado.web import Application
+from tornado.web import RequestHandler
+
+# logger
+log = get_logger()
+
+# shared memory object locks
+shared_memory_locks = {
+    "data_worker": mp.Lock(),
+    "autoignore": mp.Lock(),
+    "ongoing_hijacks": mp.Lock(),
+    "config_timestamp": mp.Lock(),
+    "time": mp.Lock(),
+}
+
+# global vars
+SERVICE_NAME = "autoignore"
+CONFIGURATION_HOST = "configuration"
+PREFIXTREE_HOST = "prefixtree"
+DATABASE_HOST = "database"
+DATA_WORKER_DEPENDENCIES = [PREFIXTREE_HOST, DATABASE_HOST]
+REST_PORT = int(os.getenv("REST_PORT", 3000))
+
+
+# TODO: move this to util
+def wait_data_worker_dependencies(data_worker_dependencies):
+    while True:
+        all_deps_met = True
+        for service in data_worker_dependencies:
+            try:
+                r = requests.get("http://{}:{}/health".format(service, REST_PORT))
+                status = True if r.json()["status"] == "running" else False
+                if not status:
+                    all_deps_met = False
+                    break
+            except Exception:
+                all_deps_met = False
+                break
+        if all_deps_met:
+            log.info("needed data workers started: {}".format(data_worker_dependencies))
+            break
+        log.info(
+            "waiting for needed data workers to start: {}".format(
+                data_worker_dependencies
+            )
+        )
+        time.sleep(1)
+
+
+def configure_autoignore(msg, shared_memory_manager_dict):
+    config = msg
+    try:
+        # check newer config
+        shared_memory_locks["config_timestamp"].acquire()
+        config_timestamp = shared_memory_manager_dict["config_timestamp"]
+        shared_memory_locks["config_timestamp"].release()
+        if config["timestamp"] > config_timestamp:
+
+            # extract autoignore rules
+            autoignore_rules = config.get("autoignore", {})
+
+            shared_memory_locks["autoignore"].acquire()
+            shared_memory_manager_dict["autoignore_rules"] = autoignore_rules
+            shared_memory_locks["autoignore"].release()
+
+            shared_memory_locks["config_timestamp"].acquire()
+            shared_memory_manager_dict["config_timestamp"] = config_timestamp
+            shared_memory_locks["config_timestamp"].release()
+
+            shared_memory_locks["time"].acquire()
+            shared_memory_manager_dict["time"] = 0
+            shared_memory_locks["time"].release()
+
+            return {"success": True, "message": "configured"}
+    except Exception:
+        log.exception("exception")
+        return {"success": False, "message": "error during data worker configuration"}
+
+
+class ConfigHandler(RequestHandler):
+    """
+    REST request handler for configuration.
+    """
+
+    def initialize(self, shared_memory_manager_dict):
+        self.shared_memory_manager_dict = shared_memory_manager_dict
+
+    def post(self):
+        """
+        Cofnigures autoignore and responds with a success message.
+        :return: {"success": True | False, "message": < message >}
+        """
+        try:
+            msg = json.loads(self.request.body)
+            self.write(configure_autoignore(msg, self.shared_memory_manager_dict))
+        except Exception:
+            self.write(
+                {"success": False, "message": "error during data worker configuration"}
+            )
+
+
+class HealthHandler(RequestHandler):
+    """
+    REST request handler for health checks.
+    """
+
+    def initialize(self, shared_memory_manager_dict):
+        self.shared_memory_manager_dict = shared_memory_manager_dict
+
+    def get(self):
+        """
+        Extract the status of a service via a GET request.
+        :return: {"status" : <unconfigured|running|stopped>}
+        """
+        status = "stopped"
+        shared_memory_locks["data_worker"].acquire()
+        if self.shared_memory_manager_dict["data_worker_running"]:
+            status = "running"
+        shared_memory_locks["data_worker"].release()
+        self.write({"status": status})
+
+
+class ControlHandler(RequestHandler):
+    """
+    REST request handler for control commands.
+    """
+
+    def initialize(self, shared_memory_manager_dict):
+        self.shared_memory_manager_dict = shared_memory_manager_dict
+
+    def start_data_worker(self):
+        shared_memory_locks["data_worker"].acquire()
+        if self.shared_memory_manager_dict["data_worker_running"]:
+            log.info("data worker already running")
+            shared_memory_locks["data_worker"].release()
+            return "already running"
+        shared_memory_locks["data_worker"].release()
+        mp.Process(target=self.run_data_worker_process).start()
+        return "instructed to start"
+
+    def run_data_worker_process(self):
+        try:
+            with Connection(RABBITMQ_URI) as connection:
+                shared_memory_locks["data_worker"].acquire()
+                data_worker = AutoignoreDataWorker(
+                    connection, self.shared_memory_manager_dict
+                )
+                self.shared_memory_manager_dict["data_worker_running"] = True
+                shared_memory_locks["data_worker"].release()
+                log.info("data worker started")
+                data_worker.run()
+        except Exception:
+            log.exception("exception")
+        finally:
+            shared_memory_locks["data_worker"].acquire()
+            self.shared_memory_manager_dict["data_worker_running"] = False
+            shared_memory_locks["data_worker"].release()
+            log.info("data worker stopped")
+
+    @staticmethod
+    def stop_data_worker():
+        shared_memory_locks["data_worker"].acquire()
+        with Connection(RABBITMQ_URI) as connection:
+            with Producer(connection) as producer:
+                command_exchange = create_exchange("command", connection)
+                producer.publish(
+                    "",
+                    exchange=command_exchange,
+                    routing_key="stop-{}".format(SERVICE_NAME),
+                    serializer="ujson",
+                )
+        shared_memory_locks["data_worker"].release()
+        message = "instructed to stop"
+        return message
+
+    def post(self):
+        """
+        Instruct a service to start or stop by posting a command.
+        Sample request body
+        {
+            "command": <start|stop>
+        }
+        :return: {"success": True|False, "message": <message>}
+        """
+        try:
+            msg = json.loads(self.request.body)
+            command = msg["command"]
+            # start/stop data_worker
+            if command == "start":
+                message = self.start_data_worker()
+                self.write({"success": True, "message": message})
+            elif command == "stop":
+                message = self.stop_data_worker()
+                self.write({"success": True, "message": message})
+            else:
+                self.write({"success": False, "message": "unknown command"})
+        except Exception:
+            log.exception("Exception")
+            self.write({"success": False, "message": "error during control"})
+
+
+class Autoignore:
+    """
+    Autoignore Service.
+    """
+
+    def __init__(self):
+        # initialize shared memory
+        shared_memory_manager = mp.Manager()
+        self.shared_memory_manager_dict = shared_memory_manager.dict()
+        self.shared_memory_manager_dict["data_worker_running"] = False
+        self.shared_memory_manager_dict["autoignore_rules"] = {}
+        self.shared_memory_manager_dict["config_timestamp"] = -1
+        self.shared_memory_manager_dict["time"] = 0
+
+        log.info("service initiated")
+
+    def make_rest_app(self):
+        return Application(
+            [
+                (
+                    "/config",
+                    ConfigHandler,
+                    dict(shared_memory_manager_dict=self.shared_memory_manager_dict),
+                ),
+                (
+                    "/control",
+                    ControlHandler,
+                    dict(shared_memory_manager_dict=self.shared_memory_manager_dict),
+                ),
+                (
+                    "/health",
+                    HealthHandler,
+                    dict(shared_memory_manager_dict=self.shared_memory_manager_dict),
+                ),
+            ]
+        )
+
+    def start_rest_app(self):
+        app = self.make_rest_app()
+        app.listen(REST_PORT)
+        log.info("REST worker started and listening to port {}".format(REST_PORT))
+        IOLoop.current().start()
+
+
+class AutoignoreDataWorker(ConsumerProducerMixin):
+    """
+    RabbitMQ Consumer/Producer for the autoignore Service.
+    """
+
+    def __init__(
+        self, connection: Connection, shared_memory_manager_dict: Dict
+    ) -> NoReturn:
+        self.connection = connection
+        self.rule_timer_thread = None
+        self.ongoing_hijacks = {}
+        self.shared_memory_manager_dict = shared_memory_manager_dict
+
+        # wait for other needed data workers to start
+        wait_data_worker_dependencies(DATA_WORKER_DEPENDENCIES)
+
+        # EXCHANGES
+        self.autoignore_exchange = create_exchange(
+            "autoignore", connection, declare=True
+        )
+        self.hijack_exchange = create_exchange(
+            "hijack-update", connection, declare=True
+        )
+        self.command_exchange = create_exchange("command", connection, declare=True)
+
+        # QUEUES
+        self.autoignore_hijacks_rules_queue = create_queue(
+            SERVICE_NAME,
+            exchange=self.autoignore_exchange,
+            routing_key="hijacks-matching-rule",
+            priority=1,
+        )
+        self.stop_queue = create_queue(
+            "{}-{}".format(SERVICE_NAME, uuid()),
+            exchange=self.command_exchange,
+            routing_key="stop-{}".format(SERVICE_NAME),
+            priority=1,
+        )
+
+        # DB variables
+        self.ro_db = DB(
+            application_name="autoignore-readonly",
+            user=DB_USER,
+            password=DB_PASS,
+            host=DB_HOST,
+            port=DB_PORT,
+            database=DB_NAME,
+            reconnect=True,
+            autocommit=True,
+            readonly=True,
+        )
+
+        self.setup_rule_timer()
+
+        log.info("data worker initiated")
+
+    def get_consumers(self, Consumer, channel):
+        return [
+            Consumer(
+                queues=[self.autoignore_hijacks_rules_queue],
+                on_message=self.handle_autoignore_hijacks_matching_rule(),
+                prefetch_count=100,
+                accept=["ujson"],
+            ),
+            Consumer(
+                queues=[self.stop_queue],
+                on_message=self.stop_consumer_loop,
+                prefetch_count=100,
+                accept=["ujson"],
+            ),
+        ]
+
+    def setup_rule_timer(self):
+        """
+        Timer for rule check operations
+        """
+        self.rule_timer_thread = threading.Timer(
+            interval=1, function=self.check_rules_should_be_checked()
+        )
+        self.rule_timer_thread.start()
+
+    def check_rules_should_be_checked(self):
+        ongoing_hijacks_to_prefixes = {}
+        # check if we need to ask prefixtree about potential rule match
+        shared_memory_locks["autoignore"].acquire()
+        try:
+            for key in self.shared_memory_manager_dict["autoignore_rules"]:
+                interval = int(
+                    self.shared_memory_manager_dict["autoignore_rules"]["interval"]
+                )
+                if interval <= 0:
+                    continue
+                shared_memory_locks["time"].acquire()
+                self.shared_memory_manager_dict["time"] += 1
+                process_time = self.shared_memory_manager_dict["time"]
+                shared_memory_locks["time"].release()
+                if process_time % interval == 0:
+                    # do the following once for the current session
+                    if len(ongoing_hijacks_to_prefixes) == 0:
+                        try:
+                            # fetch ongoing hijack events
+                            query = (
+                                "SELECT time_started, time_last, num_peers_seen, "
+                                "num_asns_inf, key, prefix, hijack_as, type, time_detected "
+                                "FROM hijacks WHERE active = true"
+                            )
+                            entries = self.ro_db.execute(query)
+                            shared_memory_locks["ongoing_hijacks"].acquire()
+                            try:
+                                self.ongoing_hijacks = {}
+                                for entry in entries:
+                                    self.ongoing_hijacks[entry[4]] = {
+                                        "prefix": entry[5],
+                                        "time_last_updated": max(
+                                            int(entry[1].timestamp()),
+                                            int(entry[8].timestamp()),
+                                        ),
+                                        "num_peers_seen": int(entry[2]),
+                                        "num_asns_inf": int(entry[3]),
+                                        "hijack_as": int(entry[6]),
+                                        "hij_type": entry[7],
+                                    }
+                                    ongoing_hijacks_to_prefixes[entry[4]] = entry[5]
+                            except Exception:
+                                log.exception("exception")
+                            finally:
+                                shared_memory_locks["ongoing_hijacks"].release()
+                        except Exception:
+                            log.exception("exception")
+
+                    self.producer.publish(
+                        {
+                            "ongoing_hijacks_to_prefixes": ongoing_hijacks_to_prefixes,
+                            "rule_key": key,
+                        },
+                        exchange=self.autoignore_exchange,
+                        routing_key="ongoing-hijack-prefixes",
+                        serializer="ujson",
+                    )
+        except Exception:
+            log.exception("exception")
+        finally:
+            shared_memory_locks["autoignore"].release()
+            self.setup_rule_timer()
+
+    def handle_autoignore_hijacks_matching_rule(self, message):
+        message.ack()
+        payload = message.payload
+        try:
+            hijacks_matching_rule = set(payload["hijacks_matching_rule"])
+            rule_key = payload["rule_key"]
+            shared_memory_locks["autoignore"].acquire()
+            rule = self.shared_memory_manager_dict["autoignore_rules"].get(
+                rule_key, None
+            )
+            shared_memory_locks["autoignore"].release()
+
+            if not rule:
+                return
+
+            thres_num_peers_seen = rule["thres_num_peers_seen"]
+            thres_num_ases_infected = rule["thres_num_ases_infected"]
+            interval = rule["interval"]
+
+            time_now = int(time.time())
+            shared_memory_locks["ongoing_hijacks"].acquire()
+            for hijack_key in hijacks_matching_rule:
+                if hijack_key not in self.ongoing_hijacks:
+                    continue
+
+                hijack_prefix = self.ongoing_hijacks[hijack_key]["prefix"]
+                hijack_type = self.ongoing_hijacks[hijack_key]["hij_type"]
+                hijack_as = self.ongoing_hijacks[hijack_key]["hijack_as"]
+                hijack_num_peers_seen = self.ongoing_hijacks[hijack_key][
+                    "num_peers_seen"
+                ]
+                hijack_num_ases_infected = self.ongoing_hijacks[hijack_key][
+                    "num_asns_inf"
+                ]
+                time_last_updated = self.ongoing_hijacks[hijack_key][
+                    "time_last_updated"
+                ]
+
+                if (
+                    (time_now - time_last_updated > interval)
+                    and (hijack_num_peers_seen < thres_num_peers_seen)
+                    and (hijack_num_ases_infected < thres_num_ases_infected)
+                ):
+                    self.producer.publish(
+                        {
+                            "key": hijack_key,
+                            "prefix": hijack_prefix,
+                            "type": hijack_type,
+                            "hijack_as": hijack_as,
+                        },
+                        exchange=self.hijack_exchange,
+                        routing_key="ignore",
+                        priority=2,
+                        serializer="ujson",
+                    )
+        except Exception:
+            log.exception("exception")
+        finally:
+            shared_memory_locks["ongoing_hijacks"].release()
+
+    def stop_consumer_loop(self, message: Dict) -> NoReturn:
+        """
+        Callback function that stop the current consumer loop
+        """
+        message.ack()
+        self.should_stop = True
+
+
+if __name__ == "__main__":
+    # initiate autoignore service with REST
+    autoignoreService = Autoignore()
+
+    # try to get configuration upon start (it is OK if it fails, will get it from POST)
+    # (this is needed because service may restart while configuration is running)
+    try:
+        r = requests.get("http://{}:{}/config".format(CONFIGURATION_HOST, REST_PORT))
+        conf_res = configure_autoignore(
+            r.json(), autoignoreService.shared_memory_manager_dict
+        )
+        if not conf_res["success"]:
+            log.info(
+                "could not get configuration upon startup, will get via POST later"
+            )
+    except Exception:
+        log.info("could not get configuration upon startup, will get via POST later")
+
+    # start REST within main process
+    autoignoreService.start_rest_app()

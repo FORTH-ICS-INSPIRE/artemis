a2d1863fee24692ee8a84452939512a67cded192
Vasileios Kotronis <biece89@gmail.com>
2020-12-01 11:29:49 +0200

    added intended_process_states and process_states DB (63 char size) changes in migration-23

--- backend-services/database/migrate/migrations/scripts/migration_23.sql
+++ backend-services/database/migrate/migrations/scripts/migration_23.sql
@@ -30,3 +30,15 @@ ALTER TABLE hijacks ADD CONSTRAINT possible_states CHECK (
 );
 
 CREATE OR REPLACE VIEW view_hijacks AS SELECT key, type, prefix, hijack_as, num_peers_seen, num_asns_inf, time_started, time_ended, time_last, mitigation_started, time_detected, timestamp_of_config, under_mitigation, resolved, active, dormant, ignored, configured_prefix, comment, seen, withdrawn, peers_withdrawn, peers_seen, outdated, community_annotation, rpki_status FROM hijacks;
+
+DROP VIEW view_processes;
+
+ALTER TABLE process_states ALTER COLUMN name TYPE VARCHAR (63);
+
+CREATE OR REPLACE VIEW view_processes AS SELECT * FROM process_states;
+
+DROP VIEW view_intended_process_states;
+
+ALTER TABLE intended_process_states ALTER COLUMN name TYPE VARCHAR (63);
+
+CREATE OR REPLACE VIEW view_intended_process_states AS SELECT * FROM intended_process_states;

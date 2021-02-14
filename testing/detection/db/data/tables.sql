c61235fcb26bd36849e8de73a6b9b8af8fab837d
georgeepta <giwrgosepta1997@hotmail.com>
2021-02-14 14:00:41 +0200

    New changes for the dataplane_msms table and view_dataplane_msms view on testing/detection/db/data/tables.sql

--- testing/detection/db/data/tables.sql
+++ testing/detection/db/data/tables.sql
@@ -225,21 +225,23 @@ CREATE OR REPLACE VIEW view_intended_process_states AS SELECT * FROM intended_pr
 CREATE OR REPLACE VIEW view_db_details AS SELECT version, upgraded_on FROM db_details;
 
 CREATE TABLE IF NOT EXISTS dataplane_msms (
-    key VARCHAR ( 32 ) NOT NULL,
+    key VARCHAR (32) NOT NULL,
     hijack_key VARCHAR(32) NOT NULL,
-    valid_origin_AS BIGINT,
-    hijack_AS BIGINT,
-    dst_addr inet,
-    msm_type VARCHAR(10),
     msm_id BIGINT NOT NULL,
-    msm_link text,
+    msm_type VARCHAR(10),
+    msm_protocol VARCHAR(4),
+    msm_start_time TIMESTAMP,
+    msm_stop_time TIMESTAMP,
+    target_ip inet,
+    num_of_probes BIGINT,
+    hijacker_AS BIGINT,
     responding VARCHAR(3) DEFAULT 'NA',
     hijacked VARCHAR(3) DEFAULT 'NA',
     PRIMARY KEY (key),
     UNIQUE (key, hijack_key, msm_id)
 );
 
-CREATE OR REPLACE VIEW view_dataplane_msms AS SELECT hijack_key, valid_origin_AS, hijack_AS, dst_addr, msm_type, msm_id, msm_link, responding, hijacked FROM dataplane_msms;
+CREATE OR REPLACE VIEW view_dataplane_msms AS SELECT hijack_key, msm_id, msm_type, msm_protocol, msm_start_time, msm_stop_time, target_ip, num_of_probes, hijacker_AS, responding, hijacked FROM dataplane_msms;
 
 CREATE FUNCTION search_bgpupdates_as_path(as_paths BIGINT[])
 RETURNS SETOF view_bgpupdates AS $$

0a4623261e0723ecfba1ba5c398e5cd2056c7568
CuriouzK0d3r <curiouz_k0d3r@protonmail.com>
2023-06-13 11:28:17 +0300

    unmitigate

--- poc_mitigate_deaggregate/poc_unmitigate.py
+++ poc_mitigate_deaggregate/poc_unmitigate.py
@@ -7,9 +7,7 @@ from ipaddress import ip_network as str2ip
 from socketIO_client import BaseNamespace
 from socketIO_client import SocketIO
 
-
 EXA_ROUTE_COMMAND_HOST = "exabgproutecommander:5000"
-
 
 log = logging.getLogger("artemis")
 log.setLevel(logging.DEBUG)
@@ -21,7 +19,6 @@ formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
 handler.setFormatter(formatter)
 # add the handlers to the logger
 log.addHandler(handler)
-
 
 # Sample call: python poc_mitigate_deaggregate.py -i '{"key":"123","prefix":"10.0.0.0/23"}'
 parser = argparse.ArgumentParser(description="test ARTEMIS mitigation via deggregation")
@@ -35,8 +32,8 @@ parser.add_argument(
 )
 parser.add_argument(
     "-p",
-    "--announced_prefixes",
-    dest="announced_prefixes",
+    "--announce_prefixes",
+    dest="announce_prefixes",
     type=str,
     help="prefixes to be announced for mitigation",
     required=False,
@@ -53,30 +50,17 @@ try:
     if args.announce_prefixes:
         announce_prefixes = json.loads(args.announce_prefixes)
 
-    log.info("Preparing to mitigate via deaggregation hijack {}".format(info_hijack))
-    hijacked_prefix = str2ip(info_hijack["prefix"])
-    hijacked_prefix_len = hijacked_prefix.prefixlen
-    deagg_len_threshold = 24
-    if hijacked_prefix.version == 6:
-        deagg_len_threshold = 64
-    if hijacked_prefix_len < deagg_len_threshold:
-        subnets = list(map(str, list(hijacked_prefix.subnets())))
-        log.info("Subnets to announce: {}".format(subnets))
-        if len(announce_prefixes) > 0:
-            subnets = announced_prefixes
+    log.info("Preparing to unmitigate hijack {}".format(info_hijack))
+    hijacked_prefix = info_hijack["prefix"]
 
-        for subnet in subnets:
-            exa_command = "announce route {} next-hop self".format(subnet)
-            sio = SocketIO("http://" + EXA_ROUTE_COMMAND_HOST, namespace=BaseNamespace)
-            sio.connect()
-            sio.emit("route_command", {"command": exa_command})
-            sio.disconnect()
-    else:
-        log.info(
-            "Cannot deaggregate a prefix more specific than /{}".format(
-                deagg_len_threshold - 1
-            )
-        )
+    log.info("Prefix to announce: {}".format(hijacked_prefix))
+
+    exa_command = "announce route {} next-hop self".format(hijacked_prefix)
+    sio = SocketIO("http://" + EXA_ROUTE_COMMAND_HOST, namespace=BaseNamespace)
+    sio.connect()
+    sio.emit("route_command", {"command": exa_command})
+    sio.disconnect()
+
 except Exception as e:
     log.error("Exception occurred while deaggregating")
     log.error(e)
